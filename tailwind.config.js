f69af5b1b56edf365d47dd88ea79ac50e7954262
Zainal Hasan <zhanang19@gmail.com>
2019-11-07 07:07:12 +0700

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    theme: {
+        extend: {
+            colors: {
+                primary: '#5b86e5',
+                secondary: '#e0e2ef'
+            }
+        }
+    },
+    variants: {},
+    plugins: []
+};
